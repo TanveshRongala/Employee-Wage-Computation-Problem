b3f34d08eb248de244728a1562c0668c7e402e5f
TanveshRongala <tanveshrongala@gmail.com>
2021-07-08 08:44:01 +0530

    Commited

--- src/EmpWageBuilderUC1.java
+++ src/EmpWageBuilderUC1.java
@@ -3,32 +3,16 @@ public class EmpWageBuilderUC1 {
 	
 public static void main(String[] args)	
 	{
-	int isFullTime=1;
-	int isPartTime=2;
-	int WagePerHr=20;
-	int FullDayHr=8;
-	int empHr;
+	int IS_FULL_TIME = 1;
 		
-	double empCheck = Math.floor(Math.random()*10)%3;
-	if(empCheck == isFullTime)
-	{
-		System.out.println("Employee is Present");
-	empHr=8;
-		
-		
-	}
-	else if(empCheck == isPartTime)
-	{
-		System.out.println("Employee is Absent");
-		empHr=4;
-	}else
-	{
-		empHr=0;
-		
-	}
+	double empCheck = Math.floor(Math.random() * 10) % 2;
 	
-	int DailyEmpWage=WagePerHr*empHr;
-	System.out.println("dailywage" + DailyEmpWage);
-
-}
+	if(empCheck == IS_FULL_TIME)
+		
+		System.out.println("Employee is Present");
+	
+	else
+		
+		System.out.println("Employee is Absent");
+	}
 }
